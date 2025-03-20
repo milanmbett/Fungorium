b3b84f28798d8a7885eae8e90e56a0f59ccc03e3
milanmbett <milanmbett@gmail.com>
2025-03-20 17:30:11 +0100

    Thread_Class kész?

--- Thread_Class.java
+++ Thread_Class.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Thread_Class 
 {
     private Tecton_Class tecton;
@@ -18,7 +22,27 @@ public class Thread_Class
     }
     public void expand_Thread()
     {
-
+        //menjen végig a tektonja szomszédsági listáján és ha van szabad hely, akkor hozzon létre egy új fonalat véletlenszerűen
+        
+        if(tecton.get_TectonNeighbours().size()==0)
+        {
+            return;
+        }
+        List<Tecton_Class> threadlessTectonNeighbours = new ArrayList<>();
+        for(Tecton_Class t : tecton.get_TectonNeighbours())
+        {
+            if(t.get_Thread().equals(null))
+            {
+                threadlessTectonNeighbours.add(t);
+            } 
+        }
+        if(threadlessTectonNeighbours.size()==0)
+        {
+            return;
+        }
+        Random random = new Random();
+        int rand = random.nextInt(threadlessTectonNeighbours.size());
+        threadlessTectonNeighbours.get(rand).set_Thread(new Thread_Class(threadlessTectonNeighbours.get(rand)));
     }
     public void die_Thread()
     {
