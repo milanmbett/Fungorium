47f15d4f566ce80c6184f60dc109929e278e9260
Killer Balázs <killerbalazs58@gmail.com>
2025-04-27 19:59:46 +0200

    fixes

--- fungorium/src/main/java/com/coderunnerlovagjai/app/Insect_Stinger.java
+++ fungorium/src/main/java/com/coderunnerlovagjai/app/Insect_Stinger.java
@@ -14,11 +14,11 @@ public class Insect_Stinger extends Insect_Class
         tecton = targetTecton;
         tecton.get_InsectsOnTecton().add(this);
         owner = p;
-        ID = "Insect_Stinger" + Integer.toString(Plane.InsectCollection.size());
+        ID = "Insect_Stinger" + Integer.toString(owner.getGame().getPlane().InsectCollection.size());
         cost = 20;
         INSECT_STINGER_LOGGER.log(Level.forName("CREATE",401),"Insect_Stinger Created! ID: " + ID + " on Tecton: " + tecton.get_ID());
-        Plane.InsectCollection.add(this);
-        INSECT_STINGER_LOGGER.log(Level.forName("ADD", 403), "Insect_Stinger: "+ID+ " added to InsectCollection! InsectCollection size: " + Plane.InsectCollection.size());
+        owner.getGame().getPlane().InsectCollection.add(this);
+        INSECT_STINGER_LOGGER.log(Level.forName("ADD", 403), "Insect_Stinger: "+ID+ " added to InsectCollection! InsectCollection size: " + owner.getGame().getPlane().InsectCollection.size());
 
     }
     public Insect_Stinger(Tecton_Class targetTecton, int hp, int ad, int as, Player p)
@@ -28,7 +28,7 @@ public class Insect_Stinger extends Insect_Class
         availableSteps = as;
         tecton = targetTecton;
         tecton.get_InsectsOnTecton().add(this);
-        Plane.InsectCollection.add(this);
+        owner.getGame().getPlane().InsectCollection.add(this);
         owner = p;
     }
     @Override
@@ -36,6 +36,6 @@ public class Insect_Stinger extends Insect_Class
     {
         Insect_Stinger duplicated = new Insect_Stinger(tecton, hp, attackDamage, availableSteps, owner);
         tecton.get_InsectsOnTecton().add(duplicated);
-        Plane.InsectCollection.add(duplicated);
+        owner.getGame().getPlane().InsectCollection.add(duplicated);
     }
 }
--- fungorium/src/main/java/com/coderunnerlovagjai/app/Mushroom_Class.java
+++ fungorium/src/main/java/com/coderunnerlovagjai/app/Mushroom_Class.java
@@ -181,7 +181,7 @@ public abstract class Mushroom_Class
     }
     public void set_Owner(Player p)
     {
-        if (p != Game.player1 && p != Game.player2) { // Ellenőrzés, hogy érvényes játékos-e
+        if (p != owner.getGame().getPlayer1() && p != owner.getGame().getPlayer2()) { // Ellenőrzés, hogy érvényes játékos-e
             MUSHROOM_CLASS_LOGGER.log(Level.forName("ERROR", 404), "Invalid player!");
             return;
         }
