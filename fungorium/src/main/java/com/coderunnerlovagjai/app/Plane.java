fd8c152e5b2bfd8dc3a5b00ba018876a4b7e0286
Terék Zoltán Tamás <zoltan.t.terek@gmail.com>
2025-04-27 11:44:00 +0200

    Plane NEM statik kollekcio

--- fungorium/src/main/java/com/coderunnerlovagjai/app/Plane.java
+++ fungorium/src/main/java/com/coderunnerlovagjai/app/Plane.java
@@ -8,11 +8,11 @@ import org.apache.logging.log4j.Logger;
 public class Plane 
 {
     private final Logger PLANE_LOGGER = LogManager.getLogger(Plane.class);
-    public static final List<Basic_Spore> SporeCollection = new ArrayList<>();
-    public static final List<Tecton_Class> TectonCollection = new ArrayList<>();
-    public static final List<Insect_Class> InsectCollection = new ArrayList<>();
-    public static final List<Thread_Class> ThreadCollection = new ArrayList<>();
-    public static final List<Mushroom_Class> MushroomCollection = new ArrayList<>();
+    public final List<Basic_Spore> SporeCollection = new ArrayList<>();
+    public final List<Tecton_Class> TectonCollection = new ArrayList<>();
+    public final List<Insect_Class> InsectCollection = new ArrayList<>();
+    public final List<Thread_Class> ThreadCollection = new ArrayList<>();
+    public final List<Mushroom_Class> MushroomCollection = new ArrayList<>();
     //TODO: ID-k jobban kell hogy működjenek ,mert jelenleg nem biztos ,hogy egyediek lesznek!
     //TODO: Páya kialakítása, elrendezése
     private Tecton_Base base1;
