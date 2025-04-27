a2ba52b066ec927fd2bba8268be2f3a147bee5ef
Zalka Mirkó Ádám <zalka.mirko.adam@gmail.com>
2025-04-27 22:20:26 +0200

    spórák javítása

--- fungorium/src/main/java/com/coderunnerlovagjai/app/Spore_Paralysing.java
+++ fungorium/src/main/java/com/coderunnerlovagjai/app/Spore_Paralysing.java
@@ -9,7 +9,7 @@ public class Spore_Paralysing extends Basic_Spore
 
     public Spore_Paralysing(Player player)
     {
-        Player owner = player;
+        owner = player;
         SPORE_PARALYSING_LOGGER.log(Level.forName("INIT",402),"Spore_Paralysing Constructor called!"); 
     }
 
@@ -18,7 +18,7 @@ public class Spore_Paralysing extends Basic_Spore
         timeToLive = 3; //_TMP value
         tecton = targetTecton;
         tecton.set_Spore(this);
-        Player owner = player;
+        owner = player;
         ID = "Spore_Paralysing" + Integer.toString(owner.getGame().getPlane().SporeCollection.size());
         SPORE_PARALYSING_LOGGER.log(Level.forName("CREATE",401),"Spore_Paralysing Created! ID: " + ID + " on Tecton: " + tecton.get_ID());
         owner.getGame().getPlane().SporeCollection.add(this);
