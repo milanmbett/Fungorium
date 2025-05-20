7d3a7fc8a47a1027e1b4291bcb7489ed31d0b57e
Terék Zoltán Tamás <zoltan.t.terek@gmail.com>
2025-05-21 00:53:48 +0200

    BaseTecton hal->jatek vege FIGYELJ DEMOBA PLAYER2VEL TESZTxd

--- fungorium/src/main/java/com/coderunnerlovagjai/app/util/LeaderboardManager.java
+++ fungorium/src/main/java/com/coderunnerlovagjai/app/util/LeaderboardManager.java
@@ -1,17 +1,39 @@
+
+// LeaderboardManager: Segédosztály a ponttáblázat (leaderboard) kezeléséhez
+// Felelős a játékosok eredményeinek mentéséért egy fájlba
 package com.coderunnerlovagjai.app.util;
 
-import java.io.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.swing.JOptionPane;
 
+
+/**
+ * Segédosztály a ponttáblázat (leaderboard) kezeléséhez.
+ * Felelős a játékosok eredményeinek mentéséért egy helyi fájlba.
+ */
 public class LeaderboardManager {
+    /**
+     * Elmenti a játékos eredményét a leaderboard.txt fájlba a felhasználó home könyvtárában.
+     * Ha a könyvtár vagy a fájl nem létezik, létrehozza azokat.
+     * @param playerName A játékos neve
+     * @param score      A játékos pontszáma
+     * @param destroyedBase Igaz, ha a játékos elpusztította az ellenség bázisát
+     */
     public static void save(String playerName, int score, boolean destroyedBase) {
+        // A leaderboard fájl elérési útja: felhasználó home/.fungorium/leaderboard.txt
         String dir = System.getProperty("user.home") + File.separator + ".fungorium";
-        new File(dir).mkdirs();
+        new File(dir).mkdirs(); // Könyvtár létrehozása, ha nem létezik
         File file = new File(dir, "leaderboard.txt");
         try (PrintWriter pw = new PrintWriter(new FileWriter(file, true))) {
+            // Sor formátuma: név,pontszám,elpusztította-e-a-bázist (pl. "Anna,120,true")
             pw.printf("%s,%d,%b%n", playerName, score, destroyedBase);
         } catch(IOException e) {
+            // Hiba esetén felugró ablakban jelezzük a hibát
             JOptionPane.showMessageDialog(null, "Could not save score: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
